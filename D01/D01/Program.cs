3549f8ee703eda86c3e324a10416d6d85bbdab23
roland31x <roland31x@gmail.com>
2023-07-22 13:37:57 +0300

    day 1 done

---
+++ D01/D01/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace D01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            List<int> fq = new List<int>();
+            fq.Add(sum);
+            int firstrepeat = int.MinValue;
+            using(StreamReader sr = new StreamReader(@"..\..\..\input.txt"))
+            {
+                while(!sr.EndOfStream)
+                {
+                    int val = int.Parse(sr.ReadLine()!);
+                    sum += val;
+                    if (fq.Contains(sum))
+                    {
+                        if (firstrepeat == int.MinValue)
+                            firstrepeat = sum;
+                    }                      
+                    else
+                        fq.Add(sum);
+                }
+            }
+            Console.WriteLine("Part 1 solution:");
+            Console.WriteLine(sum);
+            do
+            {
+                using (StreamReader sr = new StreamReader(@"..\..\..\input.txt"))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        int val = int.Parse(sr.ReadLine()!);
+                        sum += val;
+                        if (fq.Contains(sum))
+                        {
+                            firstrepeat = sum;
+                            break;
+                        }
+                        else
+                            fq.Add(sum);
+                    }
+                }
+            } while(firstrepeat == int.MinValue);
+            Console.WriteLine("Part 2 solution:");
+            Console.WriteLine(firstrepeat);
+        }
+    }
+}
